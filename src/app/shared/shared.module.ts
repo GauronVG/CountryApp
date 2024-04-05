e2e15faed437fd92f49e2cb4dd9825aeaa4414fe
Armando <Armando.villafuerte.ibm.com>
2024-04-05 16:27:33 -0600

    test

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HomePageComponent,
+    AboutPageComponent
   ]
+
 })
 export class SharedModule { }
