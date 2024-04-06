b3170a9119f62a6de2ab1a7c7e6af2fcfa24bf6c
Armando <Armando.villafuerte.ibm.com>
2024-04-05 18:24:34 -0600

    update parte 1

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,31 @@
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
 import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 
 })
